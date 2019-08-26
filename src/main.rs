85d509503b7b9620cb1bfb5e667f8fa362aa7e92
Rybakov Denis <drybakov@ozon.ru>
2019-08-26 21:54:31 +0300

    config crate usage and simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate config;
+use std::net::TcpListener;
+
+use std::path::Path;
+use config::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut settings = Config::default();
+    settings.merge(File::from(Path::new("config/config.json"))).unwrap();
+
+    let address = format!("127.0.0.1:{}", settings.port); 
+    println!("\n{:?} \n\n-----------", address);
+
+    let listener = TcpListener::bind(address).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
